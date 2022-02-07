428f4a1b458e73741a77d41dbaff42bff78a7710
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-02-07 23:21:07 +0530

    `ᴍɪʟʟɪᴇ`

--- plugins/listtod.js
+++ plugins/listtod.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m, { conn, isOwner }) => {
-  let mods = global.tod.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-  conn.reply(m.chat, `┌〔 Truth or Dare 〕\n├   *Currently playing* ` + `\n` + mods.map(v => isOwner ? '├ @' + v.replace(/@.+/, '') : '│ ' + conn.getName(v)).join`\n` + '\n└────', m, { contextInfo: { mentionedJid: tod } })
+  let tod = global.tod.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
+  conn.reply(m.chat, `┌〔 Truth or Dare 〕\n├   *Currently playing* ` + `\n` + tod.map(v => isOwner ? '├ @' + v.replace(/@.+/, '') : '│ ' + conn.getName(v)).join`\n` + '\n└────', m, { contextInfo: { mentionedJid: tod } })
 }
 handler.help = ['listtod']
 handler.tags = ['fun']
