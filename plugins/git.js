9b9acff0831235e6c54b9286a8df97cc9434da90
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-02-09 11:19:01 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -2,7 +2,7 @@ let fetch = require ('node-fetch')
 let handler = async (m, { conn }) => {
     let devil = `ㅤ
 `.trim()
-    await conn.send2ButtonLoc(m.chat, await (await fetch("https://raw.githubusercontent.com/Neeraj-x0/Millie/main/Millie/millie.jpg")).buffer(), devil, 'ᴍɪʟʟɪᴇ ʙʏ ɴᴇᴇʀᴀᴊ\nᴛʜᴀɴᴋs ᴛᴏ ᴀᴊᴍᴀʟ-ᴀᴄʜᴜ & ᴅᴇᴠɪʟ sᴇʀ\n\nhttps://github.com/Neeraj-x0/Millie \n\n\ © ᴍɪʟʟɪᴇ' , '❤️', '.❤️', '🙂', ',🙂')
+    await conn.send2ButtonLoc(m.chat, await (await fetch("https://raw.githubusercontent.com/Neeraj-x0/Millie/main/Millie/millie.jpg")).buffer(), devil, 'ᴍɪʟʟɪᴇ ʙʏ ɴᴇᴇʀᴀᴊ\nᴛʜᴀɴᴋs ᴛᴏ ᴀᴊᴍᴀʟ-ᴀᴄʜᴜ & ᴅᴇᴠɪʟ sᴇʀ\n\nhttps://github.com/Neeraj-x0/Millie \n\n\ © ᴍɪʟʟɪᴇ' , 'MENU', '.listt', 'OWNER', '.owner')
 }
 handler.tags = ['main']
 handler.help = ['git']
