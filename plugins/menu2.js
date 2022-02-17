1f7e1f3ca066e684f4bf794a7f0e303bab27229c
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-02-17 18:27:33 +0530

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -1,5 +1,5 @@
 let handler = async (m, {conn}) => {
-    conn.send2ButtonLoc(m.chat, require('fs').readFileSync('./Millie/millie.jpg'),`             💋 ᴍɪʟʟɪᴇ 💋`,`\n\n\n» ᴍɪʟʟɪᴇ ɪs ᴀ ᴍᴏᴅᴜʟᴀʀ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ᴡʀɪᴛᴛᴇɴ ɪɴ ɴᴏᴅᴇ.ᴊs\n\n» ᴊᴏɪɴ ᴍɪʟʟɪᴇ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴀɴʏ ᴋɪɴᴅ ᴏғ ʜᴇʟᴘ\n\nhttps://chat.whatsapp.com/BcOf8vB5KAlFQhb3c5d06b
+    conn.send2ButtonLoc(m.chat, require('fs').readFileSync('./Millie/millie.jpg'),`             💋 ᴍɪʟʟɪᴇ 💋 \n\n\n» ᴍɪʟʟɪᴇ ɪs ᴀ ᴍᴏᴅᴜʟᴀʀ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ᴡʀɪᴛᴛᴇɴ ɪɴ ɴᴏᴅᴇ.ᴊs\n\n» ᴊᴏɪɴ ᴍɪʟʟɪᴇ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴀɴʏ ᴋɪɴᴅ ᴏғ ʜᴇʟᴘ\n\nhttps://chat.whatsapp.com/BcOf8vB5KAlFQhb3c5d06b
 `,watermark, 'Menu', `.listt`,'Owner',`.owner`)
     }
     handler.help = ['menu']
