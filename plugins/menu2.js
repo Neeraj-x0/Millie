b1ac12ba141c66d3a43e09325a726b6920e710d4
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-02-19 09:51:37 +0530

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -2,9 +2,9 @@ let handler = async (m, {conn}) => {
     conn.send2ButtonLoc(m.chat, require('fs').readFileSync('./Millie/millie.jpg'),`             💋 ᴍɪʟʟɪᴇ 💋 \n\n\n» ᴍɪʟʟɪᴇ ɪs ᴀ ᴍᴏᴅᴜʟᴀʀ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ᴡʀɪᴛᴛᴇɴ ɪɴ ɴᴏᴅᴇ.ᴊs\n\n» ᴊᴏɪɴ ᴍɪʟʟɪᴇ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴀɴʏ ᴋɪɴᴅ ᴏғ ʜᴇʟᴘ\n\nhttps://chat.whatsapp.com/BcOf8vB5KAlFQhb3c5d06b
 `,watermark, 'Menu', `.listt`,'Owner',`.owner`)
     }
-    handler.help = ['menu']
+    handler.help = ['menu','list','help']
     
-    handler.command = /^(menu)$/i
+    handler.command = /^(menu|list|help)$/i
     module.exports = handler
 
 
