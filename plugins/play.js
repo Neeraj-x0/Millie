17a5c3a9a225eb5439fe190fe9ef5843b8e72fac
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-03-19 10:08:12 +0530

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -23,12 +23,12 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
     }
   } 
   let { dl_link, thumb, title, filesize, filesizeF } = yt
-  await conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
+  await conn.send2ButtonImg(m.chat, await (await fetch(thumb)).buffer(), `
 *Title:* ${title}
 *Audio File Size:* ${filesizeF}
 *Video File Size:* ${yt2.filesizeF}
 *Play Doesnt Work ,In Disappearing mode*
-`.trim(), watermark, '🎵ᴀᴜᴅɪᴏ', `.yta ${vid.url}`, '🎥ᴠɪᴅᴇᴏ', `.ytv ${vid.url}`)
+`.trim(), watermark, '🎶 ᴀᴜᴅɪᴏ', `.yta ${vid.url}`, '▶ ᴠɪᴅᴇᴏ', `.ytv ${vid.url}`)
 }
 handler.help = ['song','play','?'].map(v => v + ' <query>')
 handler.tags = ['downloader']
@@ -37,4 +37,3 @@ handler.command = /^(play|song)$/i
 handler.exp = 0
 
 module.exports = handler
-
