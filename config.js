bbba17d93803b3df2ebc940670d0eb54c8f326b9
Minnalsir <102296217+Minnalsir@users.noreply.github.com>
2022-05-29 16:11:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['918113921898'] // Put your number here
+global.owner = ['919400900501'] // Put your number here
 global.mods =  JSON.parse(fs.readFileSync('./src/moderator.json'))// Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium users do not need a limit
 global.tod = JSON.parse(fs.readFileSync('./src/tod.json'))
@@ -33,7 +33,7 @@ global.APIKeys = { // The APIKey is here
 
 // Sticker WM
 
-global.packname = '💋 ᴍɪʟʟɪᴇ 💋'
+global.packname = '💋 minnalser'
 
 
 global.wait = '_*Please Wait..*_'
